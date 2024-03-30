5ffed1fa3958e7d744d31dc786199abce3e1753d
Devagya Tak <thegreattak94@gmail.com>
2024-03-30 11:31:53 +0530

    Added link in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 import React from 'react'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
 
 
 
@@ -14,6 +15,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage/>}/>
+      <Route path='/jobs' element={<JobsPage/>}/>
     </Route>
   
 
